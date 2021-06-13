11d9677ea116f4faffb79e5df50bb736e1116920
imranfadhil <54136165+imranfadhil@users.noreply.github.com>
2021-06-13 11:24:20 +0800

    First MVP

--- src/logASCII_viewer/utils/LAS_handler.py
+++ src/logASCII_viewer/utils/LAS_handler.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu Dec 24 15:35:42 2020
 
@@ -10,29 +9,51 @@ import pandas as pd
 import numpy as np
 from tkinter import filedialog
 from tkinter import *
-
+import streamlit as st
 from io import StringIO
 
 
-def read_las_file(las_file):
+def read_las_file(las_file, unit='metric'):
     merged_data = pd.DataFrame()
+    header_data = pd.DataFrame()
     for f in las_file:
         bytes_data = f.read()
         str_io = StringIO(bytes_data.decode('Windows-1252'))
         las = lasio.read(str_io)
         df = las.df()
+
+        if unit == 'imperial':
+            df['DEPTH'] = las.depth_ft
+        elif unit == 'metric':
+            df['DEPTH'] = las.depth_m
+
         nullValue = las.well.NULL['value']
         df = df.where(df != nullValue, np.nan)
-        df.insert(0, 'WELLNAME', las.well.WELL['value'])
-        df.index = np.around(df.index.tolist(), 1)
-        df['DEPTH'] = df.index
+
+        wellname = las.well.WELL['value']
+        df.insert(0, 'WELLNAME', wellname)   
+
+        fieldname = las.well.FLD['value']
+        if fieldname is not '':
+            df.insert(0, 'FIELDNAME', fieldname)
+        else:
+            fieldname = wellname.split('-')[0]
+            df.insert(0, 'FIELDNAME', fieldname)
+        
         merged_data = merged_data.append(df)
+
+        well_dict = [{x:las.well[x]['value'] for x in las.well.keys()}]
+        well_header = pd.DataFrame(well_dict)        
+        
+        header_data = header_data.append(well_header)
+    
+    header_data_cols = header_data.columns
+    header_data = header_data.transpose()
+    header_data.rename({0:wellname}, axis=1, inplace=True)
+    header_data.rename({x:v for x,v in enumerate(header_data_cols)}, axis=0, inplace=True)
+
     merged_data.reset_index(inplace=True, drop=True)
-    return merged_data
-
-
-def get_header_info():
-    return
+    return merged_data, header_data
 
 
 if __name__ == '__main__':
@@ -62,4 +83,4 @@ if __name__ == '__main__':
     dfc.reset_index(inplace=True)
     print('\n ...Finish reading %d LAS files...\n' % num)
 
-    dfc.to_parquet('../data/LOGIC_LAS.pqt')
+    dfc.to_parquet('../data/combined_LAS.pqt')
--- src/logASCII_viewer/utils/LAS_visualization.py
+++ src/logASCII_viewer/utils/LAS_visualization.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import streamlit as st
 from plotly.subplots import make_subplots
 import plotly
 import plotly.graph_objects as go
@@ -12,75 +13,73 @@ plt.rcParams.update({
                     })
 
 
-def plotly_log(df, cols):
-    feat = cols
-    track = len(cols)
+def plotly_log(df, cols, unit='imperial'):
+    track = 3
     index = df.DEPTH
-    fig = make_subplots(rows=1, cols=track, shared_yaxes=True, horizontal_spacing=0.03,
-                        specs=[list([{'secondary_y': True}]*track)]
-                        )
+
+    fig = make_subplots(rows=1, cols=track, shared_yaxes=True, horizontal_spacing=0.05,
+                        specs=[list([{'secondary_y': True}]*track)] )
 
     fig.add_trace(
-        go.Scatter(x=df[feat[0]], y=index, name='GR',
+        go.Scatter(x=df[cols[0]], y=index, name='GR',
                    line_color='green'), row=1, col=1)
-    fig.update_xaxes(range=[0, 150], row=1, col=1)
 
     fig.add_trace(
-        go.Scatter(x=df[feat[1]], y=index, name='RESISTIVITY',
+        go.Scatter(x=df[cols[1]], y=index, name='RESISTIVITY',
                    line_color='red'), row=1, col=2)
-    fig.update_xaxes(range=[np.log10(.2), np.log10(200)],
-                     type='log', row=1, col=2)
 
     fig.add_trace(
-        go.Scatter(x=df[feat[2]], y=index, name='DENSITY',  # fill='tonextx',
-                   line_color='blue', ), row=1, col=3, secondary_y=False)
-    fig.update_xaxes(range=[1.85, 2.85], row=1, col=3)
+        go.Scatter(x=df[cols[2]], y=index, name='DENSITY', 
+                   line_color='blue', fill='tonextx', fillcolor='rgba(0,0,255,.1)'), 
+                   row=1, col=3, secondary_y=False)
 
     fig.add_trace(
-        go.Scatter(x=df[pred], y=index, name=pred, line_color='orange',
-                   fill='tozerox'), row=1, col=4)
-    fig.update_xaxes(range=[0, np.max(df[pred])], row=1, col=4)
-
-    fig.add_trace(
-        go.Scatter(x=df[feat[3]], y=index, name='NEUTRON',
-                   line_color='red'), row=1, col=3, secondary_y=True)
-    fig.update_xaxes(range=[.45, -.15], row=1, col=3)
-    fig.data[4].update(xaxis='x5')
+        go.Scatter(x=df[cols[3]], y=index, name='NEUTRON',
+                   line_color='red', fill='tonextx', fillcolor='rgba(255,0,0,.1)'), 
+                   row=1, col=3, secondary_y=True)        
+    fig.data[3].update(xaxis='x4')
 
     fig.update_layout(
         xaxis1=dict(title="GR", titlefont=dict(color="green"), tickfont=dict(color="green"),
-                    side='top', anchor='free', position=.9, title_standoff=.1),
+                    side='top', anchor='free', position=.9, title_standoff=.1, range=[0,150]),
+        
         xaxis2=dict(title="RT", titlefont=dict(color="red"), tickfont=dict(color="red"),
-                    side='top', anchor='free', position=0.9, title_standoff=.1),
+                    side='top', anchor='free', position=.9, title_standoff=.1, 
+                    range=[np.log10(.2), np.log10(200)], type='log'),
+        
         xaxis3=dict(title="RHOB", titlefont=dict(color="blue"), tickfont=dict(color="blue"),
-                    side='top', anchor='free', position=.9, title_standoff=.1),
-        xaxis4=dict(title=pred, titlefont=dict(color="orange"), tickfont=dict(color="orange"),
-                    side='top', anchor='free', position=.9, title_standoff=.1),
-        xaxis5=dict(title="NPHI", titlefont=dict(color="red"), tickfont=dict(color="red"),
-                    side='top', anchor='free', position=.97, title_standoff=.1, overlaying='x3'),
-    )
-
-    # make room to display double x-axes
-    fig.update_layout(
-        yaxis=dict(domain=[0, .9]),
+                    side='top', anchor='free', position=.9, title_standoff=.1, 
+                    range=[1.85,2.85]),         
+        
+        xaxis4=dict(title="NPHI", titlefont=dict(color="red"), tickfont=dict(color="red"),
+                    side='top', anchor='free', position=.97, title_standoff=.1, overlaying='x3',
+                    range=[.45, -.15]),
+                    
+        # make room to display double x-axes    
+        yaxis=dict(domain=[0, .9], title='DEPTH '+'(ft MD)' if unit=='imperial' else '(m MD)'),
         yaxis2=dict(domain=[0, .9]),
-        yaxis3=dict(domain=[0, .9]),
-        yaxis4=dict(domain=[0, .9]),
-        yaxis5=dict(domain=[0, .9]),
+        yaxis3=dict(domain=[0, .9], visible=False),
+        yaxis4=dict(domain=[0, .9], visible=False),
+        yaxis5=dict(domain=[0, .9], visible=False),
         yaxis6=dict(domain=[0, .9], visible=False),
-        yaxis7=dict(domain=[0, .9]),
-        yaxis8=dict(domain=[0, .9]),
+
+        height=900, 
+        width=800, 
+        margin=dict(t=20), 
+        showlegend=False,                      
+        title={
+            'text': "%s Logs" % df.WELLNAME.unique()[0],
+            'y': .99,
+            'x': 0.09,
+            'xanchor': 'left',
+            'yanchor': 'top', 
+        }
     )
 
-    fig.update_xaxes(fixedrange=True)
+    fig.update_xaxes(
+        fixedrange=True
+    )    
     fig.update_yaxes(matches='y', constrain='domain', autorange="reversed")
-    fig.update_layout(height=900, width=900, margin=dict(t=25),
-                      title={
-        'text': "%s Logs" % df.WELLNAME.unique()[0],
-        'y': 1,
-        'x': 0,
-        'xanchor': 'left',
-        'yanchor': 'top',
-    }
-    )
-    return(fig)
+    
+    st.write(fig)
+    return fig
